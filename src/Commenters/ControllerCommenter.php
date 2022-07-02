5b5ee6ac04aaec32190f1895728f900f434894e5
Freek Van der Herten <freek@spatie.be>
2022-07-02 02:59:24 +0200

    wip

--- src/Commenters/ControllerCommenter.php
+++ src/Commenters/ControllerCommenter.php
@@ -16,27 +16,42 @@ class ControllerCommenter implements Commenter
     /** @return Comment|array<Comment>|null */
     public function comments(string $query, Connection $connection): Comment|array|null
     {
-        if (! request()->route()) {
+        if (!request()->route()) {
             return null;
         }
 
+        [$controller, $action] = $this->getControllerAndAction();
+
+        return [
+            new Comment('controller', $controller),
+            new Comment('action', $action),
+        ];
+    }
+
+    /**
+     * @return array{
+     *            0: string,
+     *            1: string,
+     *         }
+     */
+    protected function getControllerAndAction(): array
+    {
         $action = request()->route()->getAction('uses');
 
         if ($action instanceof Closure) {
             $reflection = new ReflectionClosure($action);
             $controller = 'Closure';
             $action = $reflection->getFileName();
-        } else {
-            $controller = $this->includeNamespace
-                ? explode('@', $action)[0] ?? null
-                : class_basename(explode('@', $action)[0]);
 
-            $action = explode('@', $action)[1] ?? null;
+            return [$controller, $action];
         }
 
-        return [
-            new Comment('controller', $controller),
-            new Comment('action', $action),
-        ];
+        $controller = $this->includeNamespace
+            ? explode('@', $action)[0] ?? null
+            : class_basename(explode('@', $action)[0]);
+
+        $action = explode('@', $action)[1] ?? null;
+
+        return [$controller, $action];
     }
 }
