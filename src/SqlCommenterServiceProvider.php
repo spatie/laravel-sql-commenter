212275229e9602010ed5f9f6199eac69c724d089
Freek Van der Herten <freek@spatie.be>
2022-07-02 11:43:20 +0200

    wip

--- src/SqlCommenterServiceProvider.php
+++ src/SqlCommenterServiceProvider.php
@@ -21,7 +21,7 @@ class SqlCommenterServiceProvider extends PackageServiceProvider
         $this->app->singleton(SqlCommenter::class, function () {
             $commenterClass = config('sql-commenter.commenter_class');
 
-            if (! is_a($commenterClass, SqlCommenter::class, true)) {
+            if (!is_a($commenterClass, SqlCommenter::class, true)) {
                 throw InvalidSqlCommenter::make($commenterClass);
             }
 
@@ -30,8 +30,8 @@ class SqlCommenterServiceProvider extends PackageServiceProvider
 
         $this->app->get('db.connection')
             ->beforeExecuting(function (
-                string     &$query,
-                array      &$bindings,
+                string &$query,
+                array &$bindings,
                 Connection $connection,
             ) {
                 $sqlCommenter = app(SqlCommenter::class);
